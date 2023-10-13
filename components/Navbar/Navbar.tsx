1dd3569f01444ff7e736e09a3cfffcacd012bf8a
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-10-13 12:26:19 +0100

    nav bar creted

---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,42 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+const navIcons = [
+  { src: '/assets/icons/search.svg', alt: 'Search' },
+  { src: '/assets/icons/black-heart.svg', alt: 'heart' },
+  { src: '/assets/icons/user.svg', alt: 'User' },
+];
+
+const Navbar = () => {
+  return (
+    <header className="w-full">
+      <nav className="nav">
+        <Link href="/" className="flex items-center gap-1">
+          <Image
+            src="assets/icons/logo.svg"
+            width={27}
+            height={27}
+            alt="logo"
+          />
+          <p className="nav-logo">
+            Chalk<span className="text-primary">board</span>
+          </p>
+        </Link>
+        <div className="flex items-center gap-5">
+          {navIcons.map((icon) => (
+            <Image
+              key={icon.alt}
+              src={icon.src}
+              alt={icon.alt}
+              width={27}
+              height={27}
+            />
+          ))}
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
