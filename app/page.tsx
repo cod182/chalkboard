e26424131c7e42eef5af040f15327af4901b6ac6
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-10-13 13:24:36 +0100

    start of first section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,26 @@
+import Image from 'next/image';
 import React from 'react';
 
 const Home = () => {
-  return <div>home</div>;
+  return (
+    <>
+      <section className="px-6 md:px-20 py-24 border-2 border-red-500">
+        <div className="flex max-xl:flex-col gap-16">
+          <div className="flex flex-col justify-center">
+            <p className="small-text">
+              Smart shopping starts here:
+              <Image
+                src="/assets/icons/arrow-right.svg"
+                alt="arrow right"
+                width={16}
+                height={16}
+              />
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Home;
