39c73466d9a2c96e6a66f2b375854295325425d3
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-10-13 17:44:15 +0100

    added carosuel

--- components/HeroCarousel/HeroCarousel.tsx
+++ components/HeroCarousel/HeroCarousel.tsx
@@ -1,7 +1,55 @@
-import React from 'react';
+'use client';
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import Image from 'next/image';
+
+const heroImages = [
+  {
+    imgUrl: '/assets/images/hero-1.svg',
+    alt: 'smart watch',
+  },
+  {
+    imgUrl: '/assets/images/hero-2.svg',
+    alt: 'bag',
+  },
+  {
+    imgUrl: '/assets/images/hero-3.svg',
+    alt: 'lamp',
+  },
+  {
+    imgUrl: '/assets/images/hero-4.svg',
+    alt: 'airfryer',
+  },
+  {
+    imgUrl: '/assets/images/hero-5.svg',
+    alt: 'chair',
+  },
+];
 
 const HeroCarousel = () => {
-  return <div>HeroCarousel</div>;
+  return (
+    <div className="hero-carousel">
+      <Carousel
+        showThumbs={false}
+        autoPlay
+        infiniteLoop
+        interval={2000}
+        showArrows={false}
+        showStatus={false}
+      >
+        {heroImages.map((image) => (
+          <Image
+            key={image.alt}
+            src={image.imgUrl}
+            alt={image.alt}
+            height={484}
+            width={484}
+            className="object-contain"
+          />
+        ))}
+      </Carousel>
+    </div>
+  );
 };
 
 export default HeroCarousel;
