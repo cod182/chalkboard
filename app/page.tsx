e7d3cb899408258e130d2a4e449bf24dd39b23cb
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-10-13 14:06:52 +0100

    created search bar design

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Searchbar from '@/components/Searchbar/Searchbar';
 import Image from 'next/image';
 import React from 'react';
 
@@ -24,7 +25,7 @@ const Home = () => {
               Powerful, self-serve product and growth analytics to help you
               convert, engage, and retain more.
             </p>
-            {/* Search Bar */}
+            <Searchbar />
           </div>
           {/* Hero Carosel */}
         </div>
