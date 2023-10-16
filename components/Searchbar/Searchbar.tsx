51c76bef97aceb2b643c4f2199b8f6b332fe862a
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-10-16 13:00:10 +0100

    set up link check in search func

--- components/Searchbar/Searchbar.tsx
+++ components/Searchbar/Searchbar.tsx
@@ -6,17 +6,49 @@ const isValidAmazonUrl = (url: string) => {
   try {
     const parsedUrl = new URL(url);
     const hostname = parsedUrl.hostname;
-  } catch (error) {}
-};
 
-const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-  e.preventDefault();
-
-  const isValidLink = isValidAmazonUrl(searchPrompt);
+    if (
+      hostname.includes('amazon.') ||
+      hostname.includes('amazon.co.uk') ||
+      hostname.includes('amazon.com') ||
+      hostname.endsWith('amazon')
+    ) {
+      return true;
+    }
+  } catch (error) {
+    return false;
+  }
+  return false;
 };
 
 const Searchbar = () => {
   const [searchPrompt, setSearchPrompt] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [linkProvided, setLinkProvided] = useState(false);
+  const [validLink, setValidLink] = useState(true);
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const isValidLink = isValidAmazonUrl(searchPrompt);
+    setLinkProvided(true);
+
+    if (!isValidLink) {
+      setValidLink(false);
+      return alert('Link not valid. Please enter and amazon link');
+    } else {
+      setValidLink(true);
+    }
+
+    try {
+      setIsLoading(true);
+      
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <form className="flex flex-wrap gap4 mt-12" onSubmit={handleSubmit}>
@@ -25,10 +57,20 @@ const Searchbar = () => {
         onChange={(e) => setSearchPrompt(e.target.value)}
         type="text"
         placeholder="Enter Product Link"
-        className="searchbar-input"
+        className={`searchbar-input mr-2 ${
+          linkProvided
+            ? validLink
+              ? 'border-green-400'
+              : 'border-red-600'
+            : null
+        }`}
       />
-      <button type="submit" className="searchbar-btn">
-        Search
+      <button
+        disabled={searchPrompt === ''}
+        type="submit"
+        className="searchbar-btn"
+      >
+        {isLoading ? 'Searching' : 'Search'}
       </button>
     </form>
   );
