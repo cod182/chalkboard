f3464b174a741169de6560ed0bda8dab40f87c83
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-10-16 12:45:06 +0100

    setting up search func

--- components/Searchbar/Searchbar.tsx
+++ components/Searchbar/Searchbar.tsx
@@ -1,11 +1,28 @@
 'use client';
 
-const handleSubmit = () => {};
+import { FormEvent, useState } from 'react';
+
+const isValidAmazonUrl = (url: string) => {
+  try {
+    const parsedUrl = new URL(url);
+    const hostname = parsedUrl.hostname;
+  } catch (error) {}
+};
+
+const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+  e.preventDefault();
+
+  const isValidLink = isValidAmazonUrl(searchPrompt);
+};
 
 const Searchbar = () => {
+  const [searchPrompt, setSearchPrompt] = useState('');
+
   return (
     <form className="flex flex-wrap gap4 mt-12" onSubmit={handleSubmit}>
       <input
+        value={searchPrompt}
+        onChange={(e) => setSearchPrompt(e.target.value)}
         type="text"
         placeholder="Enter Product Link"
         className="searchbar-input"
