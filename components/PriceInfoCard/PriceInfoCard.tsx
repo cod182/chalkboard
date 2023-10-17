3e548ec496287a5824975e927156ef06bd8a9984
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-10-17 15:55:21 +0100

    designed ui for reusbal price info card

--- components/PriceInfoCard/PriceInfoCard.tsx
+++ components/PriceInfoCard/PriceInfoCard.tsx
@@ -1,8 +1,10 @@
+import Image from 'next/image';
+
 declare interface InfoCardProps {
   title: string;
-  iconSrc: String;
-  value: String;
-  borderColor: String;
+  iconSrc: string;
+  value: string;
+  borderColor: string;
 }
 
 const PriceInfoCard = ({
@@ -11,9 +13,15 @@ const PriceInfoCard = ({
   value,
   borderColor,
 }: InfoCardProps) => {
-  return <div className={`price-info_card border-l-[${borderColor}]`}>
-    
-  </div>;
+  return (
+    <div className={`price-info_card border-l-[${borderColor}]`}>
+      <p className="text-base text-black-100">{title}</p>
+      <div className="flex gap-1">
+        <Image src={iconSrc} alt={title} width={24} height={24} />
+        <p className="text-2xl font-bold text-secondary">{value}</p>
+      </div>
+    </div>
+  );
 };
 
 export default PriceInfoCard;
